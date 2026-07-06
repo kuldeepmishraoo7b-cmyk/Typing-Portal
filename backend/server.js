3257ac05f18840742093d6aa40e8d61ab67fb791
kuldeepmishraoo7b-cmyk <kuldeepmishraoo7b@gmail.com>
2026-07-06 15:43:33 +0530

    Fix port for deployment

--- backend/server.js
+++ backend/server.js
@@ -902,4 +902,5 @@ app.get("/verify-session/:id", (req, res) => {
 
 
 
-app.listen(5000, () => console.log("✅ Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
